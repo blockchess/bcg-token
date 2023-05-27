db173234bd2178c4146d8c56c580ed61f418ebe9
Alex K <zhdanoff.alexey@gmail.com>
2023-05-27 17:39:49 +0700

    update test coverage to 100%

--- test/BCT.test.ts
+++ test/BCT.test.ts
@@ -2,53 +2,104 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { BCT } from "../typechain-types/contracts/BCT";
+import { BCG } from "../typechain-types/contracts/BCG";
 
-describe("Token Contract", async () => {
+describe("BSG Token Contract", async () => {
   let signers: SignerWithAddress[];
-  let BCT: BCT;
+  let BCG: BCG;
   let owner: SignerWithAddress;
   let user: SignerWithAddress;
 
   beforeEach(async () => {
-    // Get owner as signer, account as customer
+    // get owner as signer, account as customer
     signers = await ethers.getSigners();
     owner = signers[0];
     user = signers[1];
 
-    // Deploy smart contract
-    const BCTFactory = await ethers.getContractFactory("BCT");
-    BCT = (await BCTFactory.deploy()) as BCT;
-    await BCT.deployed();
+    // deploy smart contract
+    const BCGFactory = await ethers.getContractFactory("BCG");
+    BCG = (await BCGFactory.deploy()) as BCG;
+    await BCG.deployed();
   });
 
   it('Started total supply MUST BE equal to "0"', async () => {
-    expect(await BCT.totalSupply()).equal("0");
+    expect(await BCG.totalSupply()).equal("0");
   });
 
-  describe("Mint", () => {
+  it('Cap MUST BE equal to "100000000000000000000000000"', async () => {
+    expect(await BCG.cap()).equal("100000000000000000000000000");
+  });
+
+  it("Started owner MUST BE equal to deployer", async () => {
+    expect(await BCG.owner()).equal(owner.address);
+    expect(await BCG.getOwner()).equal(owner.address);
+  });
+
+  it('Name MUST BE equal to "BlockChess Game"', async () => {
+    expect(await BCG.name()).equal("BlockChess Game");
+  });
+
+  it('Symbol MUST BE equal to "BCG"', async () => {
+    expect(await BCG.symbol()).equal("BCG");
+  });
+
+  it('Decimals MUST BE equal to "18"', async () => {
+    expect(await BCG.decimals()).equal(18);
+  });
+
+  describe("Token minting", () => {
     const amountToMint = 10000;
-    beforeEach(async () => {
-      // mint amountToMint tokens
-      await BCT.mint(amountToMint, owner.address);
+
+    it('"totalSupply" MUST BE equal to minted amount', async () => {
+      await BCG.mint(amountToMint, owner.address);
+      expect(await BCG.totalSupply()).equal(amountToMint);
+
+      await BCG.mint(amountToMint, owner.address);
+      expect(await BCG.totalSupply()).equal(amountToMint * 2);
     });
 
-    it('"totalSupply" MUST BE equal to "amountToMint"', async () => {
-      expect(await BCT.totalSupply()).equal(amountToMint);
+    it('"totalSupply" MUST decrease on burning', async () => {
+      await BCG.mint(amountToMint, owner.address);
+      expect(await BCG.totalSupply()).equal(amountToMint);
+
+      await BCG.burn(amountToMint / 2);
+      expect(await BCG.totalSupply()).equal(amountToMint - amountToMint / 2);
     });
 
-    it('"Owner" MUST HAVE balance equal to "amountToMint"', async () => {
-      expect(await BCT.balanceOf(owner.address)).equal(amountToMint);
-    });
-  });
+    it('"Owner" MUST HAVE balance equal to minted amount', async () => {
+      await BCG.mint(amountToMint, owner.address);
 
-  describe("Mint over cap", () => {
-    it('MUST revert with "BEP20: cap exceeded"', async () => {
+      expect(await BCG.balanceOf(owner.address)).equal(amountToMint);
+    });
+
+    it("Any target MUST HAVE balance equal to minted amount", async () => {
+      await BCG.mint(amountToMint, user.address);
+
+      expect(await BCG.balanceOf(user.address)).equal(amountToMint);
+    });
+
+    it("MUST revert if the minter is not owner", async () => {
+      await expect(BCG.connect(user).mint(amountToMint, owner.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+      await expect(BCG.connect(user).mint(amountToMint, user.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    it("MUST revert if the cap is over", async () => {
       // get total cap
-      const cap = await BCT.cap();
+      const cap = await BCG.cap();
 
       // mint more than total cap
-      await expect(BCT.mint(cap + 1, owner.address)).to.be.revertedWith("BEP20: cap exceeded");
+      await expect(BCG.mint(cap.add(1), owner.address)).to.be.revertedWith("BCG: cap exceeded");
+      await expect(BCG.mint(cap.add(100), owner.address)).to.be.revertedWith("BCG: cap exceeded");
+      await expect(BCG.mint(cap.add(100000000000), owner.address)).to.be.revertedWith("BCG: cap exceeded");
+
+      // mint less than total cap
+      await BCG.mint(1, owner.address);
+      await BCG.mint(100, owner.address);
+      await BCG.mint(100000000000, owner.address);
     });
   });
 });
